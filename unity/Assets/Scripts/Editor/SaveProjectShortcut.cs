af632e60cfb3a9fe821803e26a98ab21d3688e8b
savam <savam@nordeus.eu>
2015-04-17 22:52:30 +0200

    Initial scene, super save handler

---
+++ unity/Assets/Scripts/Editor/SaveProjectShortcut.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[InitializeOnLoad]
+public class SaveProjectShortcut
+{
+	private const float TimeBetweenSaves = 180f;
+
+	private static float lastSaveTime = 0f;
+
+	[MenuItem("File/Super Save Project %`")]
+	public static void SaveProject()
+	{
+		if (Application.isPlaying) return;
+
+		Debug.Log("Saving everything");
+		EditorApplication.SaveScene();
+		EditorApplication.SaveAssets();
+	}
+
+	static SaveProjectShortcut()
+	{
+		lastSaveTime = 0f;
+		EditorApplication.update -= Update;
+		EditorApplication.update += Update;
+	}
+
+	public static void Update()
+	{
+		if (Application.isPlaying) return;
+
+		if (Time.realtimeSinceStartup > lastSaveTime + TimeBetweenSaves)
+		{
+			Debug.Log("Auto save");
+			EditorApplication.SaveAssets();
+			lastSaveTime = Time.realtimeSinceStartup;
+		}
+	}
+}
